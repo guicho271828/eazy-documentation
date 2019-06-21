60e86628948df568f2edaf7ca2834d15e914e7d5
Masataro Asai <guicho2.71828@gmail.com>
2019-06-21 11:35:28 +0900

    file entry

--- src/1definition.lisp
+++ src/1definition.lisp
@@ -2,10 +2,12 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
 (defclass definition ()
-  ((doctype :accessor doctype :initarg :doctype)
-   (name :accessor name :initarg :name)
-   (args :accessor args :initarg :args)
-   (docstring :accessor docstring :initarg :docstring))
+  ((doctype   :accessor   doctype :initarg :doctype   :type symbol)
+   (name      :accessor      name :initarg :name      :type symbol)
+   (args      :accessor      args :initarg :args      :type list)
+   (docstring :accessor docstring :initarg :docstring :type string)
+   (file      :accessor      file :initarg :file      :type pathname
+              :initform (or *compile-file-pathname* *load-pathname*)))
   (:documentation "Instances represententing a documentation entry."))
 )
 
@@ -16,16 +18,18 @@ Docstring are compared because the same function could be recognized multiple
 times when a macro expands into another macro (e.g. defrule -> defun).
 In such a case, the docstring objects are most likely the same object (by EQ)."
   (match* (a b)
-    (((definition :doctype (place d1) :name (place n1) :docstring (place s1))
-      (definition :doctype (place d2) :name (place n2) :docstring (place s2)))
+    (((definition :doctype (place d1) :name (place n1) :docstring (place s1) :file f1)
+      (definition :doctype (place d2) :name (place n2) :docstring (place s2) :file f2))
      (let* ((name    (eq n1 n2))
             (doctype (eq d1 d2))
+            (file    (equal f1 f2))
             (s1 (ignore-errors s1))
             (s2 (ignore-errors s2))
             (string (equal s1 s2)))
-       (or (and name (or (not s1) (not s2) string))
-           (and doctype s1 s2 string)
-           (and name doctype string))
+       (and file
+            (or (and name (or (not s1) (not s2) string))
+                (and doctype s1 s2 string)
+                (and name doctype string)))
 
        ;; wrong attempts
        #+(or)
